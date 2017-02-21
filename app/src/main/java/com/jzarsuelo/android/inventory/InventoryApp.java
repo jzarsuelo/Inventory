f035fbef2e0a6f25d248ee64cece8a71fd25ac6d
Pao <jzarsuelo.dev@gmail.com>
2017-02-21 18:27:56 +0800

    [US02] - Created InvetoryApp for easy access of Context instance

---
+++ app/src/main/java/com/jzarsuelo/android/inventory/InventoryApp.java
@@ -0,0 +1,19 @@
+package com.jzarsuelo.android.inventory;
+
+import android.app.Application;
+import android.content.Context;
+
+public class InventoryApp extends Application {
+
+    private static Context sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return sContext;
+    }
+}
